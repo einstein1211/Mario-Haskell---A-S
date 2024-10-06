278dd233b9400553d8fb7bc91c008c779a09e3b7
einstein1211 <steinvanvliet@live.nl>
2024-10-06 17:59:37 +0200

    datatypes brainstorm

--- app/Model.hs
+++ app/Model.hs
@@ -1,13 +1,50 @@
 module Model where
 
-data InfoToShow = ShowNothing
-                | ShowNumber    Int
-                | ShowChar      Char
+data Velocity = Vel Float Float deriving(Show,Eq)
 
-data GameState = GameState {
-                    infoToShow  :: InfoToShow,
-                    elapsedTime :: Float --Maybe could be Int
-                    }
+data Point = Point Float Float deriving(Show,Eq) --Could perhaps be Int,Int but would maybe cause issues with position calc
 
-initialState :: GameState
-initialState = GameState ShowNothing 0
+fps :: Int
+fps = 60
+
+data Name       = Mario | Goomba    | GreenKoopa    | RedKoopa | Spiny 
+    deriving (Show,Eq)
+data Direction  = RIGHT | LEFT 
+    deriving(Show,Eq)
+data IsAlive    = ALIVE | DEAD 
+    deriving(Show,Eq)
+data ObjName    = Coin  | Mushroom  | FireFlower 
+    deriving (Show,Eq)
+data ObjType    = COIN  | POWERUP 
+    deriving (Show,Eq)
+
+-- | Data describing characters in Game (Mario & Enemies)
+data Characters = Characters
+    {   cName :: Name
+    ,   cSpeed :: Velocity
+    ,   cDirection :: Direction
+    ,   cLocation :: Point
+    ,   cAlive :: IsAlive
+    } deriving (Show,Eq)
+
+-- | Data descriving objects in Game (Coins & Powerups)
+data Objects = Objects
+    {   oName :: ObjName
+    ,   oSpeed :: Velocity
+    ,   oDirection :: Direction
+    ,   oLocation :: Point
+    ,   oAlive :: IsAlive
+    ,   oType :: ObjType  
+    } deriving (Show,Eq)
+
+-- data InfoToShow = ShowNothing
+--                 | ShowNumber    Int
+--                 | ShowChar      Char
+
+-- data GameState = GameState {
+--                     infoToShow  :: InfoToShow,
+--                     elapsedTime :: Float --Maybe could be Int
+--                     }
+
+-- initialState :: GameState
+-- initialState = GameState ShowNothing 0
