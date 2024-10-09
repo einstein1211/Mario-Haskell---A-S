ee838a0f501c67b4fa05c508ba01c103496cd386
einstein1211 <steinvanvliet@live.nl>
2024-10-09 16:55:36 +0200

    implementation of physics for players & enemies

--- app/Controller.hs
+++ app/Controller.hs
@@ -1,23 +1,53 @@
 module Controller where
 
 import Model
-import Graphics.Gloss
+-- import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 
 step :: Float -> GameState -> IO GameState
-step = return physics
+step secs gstate = 
+  return $ physics secs gstate
 
 physics :: Float -> GameState -> GameState
-physics secs gstate = gstate {object = Object {name = RECTANGLE, location = (x',y'),shape = path, vel = Vel vx vy}}
+physics secs gstate = 
+  gstate {
+    players = map plf (players gstate),
+    enemies = map enf (enemies gstate),
+    items   = map itf (items gstate)
+    }
   where
-    (x,y) = location (object gstate)
-    Vel vx vy = vel (object gstate)
-    x' = x + vx*secs
-    y' = y + vy*secs
-    path = shape (object gstate)
+    plf obj = obj  {plyPhysics = physhelp secs (plyPhysics obj)}
+    enf obj = obj  {ePhysics   = physhelp secs (ePhysics obj)}
+    itf obj = obj  {iPhysics   = physhelp secs (iPhysics obj)}
+    physhelp :: Float -> Physics -> Physics
+    physhelp s p = colissionCheck p {pos = (x',y'), vel = (vx',vy'), acc = (ax,ay)}
+      where
+        (x,y)   = pos p
+        (vx,vy) = vel p
+        (ax,ay) = acc p
+        x'  = x   + vx*s
+        y'  = y   + vy*s
+        vx' = vx  + ax*s
+        vy' = vy  + ay*s
+    colissionCheck :: Physics -> Physics
+    colissionCheck p = p {pos = pos p, vel = (vx',vy'), acc = (ax',ay')}
+      where
+        (x,y)   = pos p
+        (vx,vy) = vel p
+        (ax,ay) = acc p
+        vx' = if x > 640 || x < (-640) then -vx else vx
+        vy' = if y > 360 || y < (-360) then -vy else vy
+        ax' = if x > 640 || x < (-640) then -ax else ax
+        ay' = if y > 360 || y < (-360) then -ay else ay
 
+-- | Handle user input
 input :: Event -> GameState -> IO GameState
-input = undefined
+-- input e gstate = return (inputKey e gstate)
+input e = return
 
 inputKey :: Event -> GameState -> GameState
-inputKey = undefined
+inputKey (EventKey (Char c) _ _ _) gstate
+  = -- If the user presses a character key, show that one
+    -- gstate { infoToShow = ShowAChar c }
+    gstate
+inputKey _ gstate = gstate -- Otherwise keep the same
