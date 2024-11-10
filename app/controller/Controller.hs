2f308e9ad6472148a25667cf50972992abb30187
astraHU <astra.hedvat@student.hu.nl>
2024-11-10 13:36:26 +0100

    removed dead code (debug step)

--- app/controller/Controller.hs
+++ app/controller/Controller.hs
@@ -11,31 +11,15 @@ import Graphics.Gloss.Interface.IO.Game
 import System.Exit (exitSuccess)
 import Debug.Trace (trace)
 
--- trace :: String -> a -> a
-
 directKey :: [SpecialKey]
 directKey = [KeyDown,KeyUp,KeyLeft,KeyRight,KeySpace,KeyShiftL]
 
--- Non debug step
 step :: Float -> GameState -> IO GameState
 step secs gstate
   | mode gstate == StartMenu = return gstate
   | isPaused gstate = return gstate
   | otherwise = return $ entityInteractions secs $ applyPhysics secs $ levelUpdate secs $ entityUpdate gstate { time = time gstate + secs }
 
---Debug step
--- step :: Float -> GameState -> IO GameState --startmenu
--- step secs gstate
---   | mode gstate == StartMenu = return gstate
---   | isPaused gstate = return gstate
---   | otherwise = return $ trace "Before entityUpdate" $
---                 trace (show (time gstate)) $
---                 entityInteractions secs $ 
---                 applyPhysics secs $ 
---                 trace "After entityUpdate" $ 
---                 entityUpdate gstate { time = time gstate + secs }
-
--- Handle s en q tijdens het start menu
 input :: Event -> GameState -> IO GameState
 input e gstate = case e of
   EventKey (Char 's') Down _ _ | mode gstate == StartMenu -> return gstate { mode = Playing }
