474f7e1d3cb6b66da36d273577a6a9098b850d5b
astraHU <astra.hedvat@student.hu.nl>
2024-11-10 20:06:32 +0100

    exit screen works 50% of the time

--- app/controller/Controller.hs
+++ app/controller/Controller.hs
@@ -10,6 +10,8 @@ import Controller.LevelUpdate
 import Graphics.Gloss.Interface.IO.Game
 import System.Exit (exitSuccess)
 import Debug.Trace (trace)
+import Control.Concurrent (threadDelay)
+
 
 -- trace :: String -> a -> a
 
@@ -21,27 +23,20 @@ step :: Float -> GameState -> IO GameState
 step secs gstate
   | mode gstate == StartMenu = return gstate
   | isPaused gstate = return gstate
+  | not (any isAlive (players gstate)) && mode gstate == Playing = return gstate {mode = Exiting}
+  | mode gstate == Exiting = do
+      putStrLn $ "Thanks for playing! Your final score is " ++ show (score gstate)
+      threadDelay 3000000
+      exitSuccess
   | otherwise = return $ entityInteractions secs $ applyPhysics secs $ levelUpdate secs $ entityUpdate gstate { time = time gstate + secs }
 
---Debug step
--- step :: Float -> GameState -> IO GameState --startmenu
--- step secs gstate
---   | mode gstate == StartMenu = return gstate
---   | isPaused gstate = return gstate
---   | otherwise = return $ trace "Before entityUpdate" $
---                 trace (show (time gstate)) $
---                 entityInteractions secs $ 
---                 applyPhysics secs $ 
---                 trace "After entityUpdate" $ 
---                 entityUpdate gstate { time = time gstate + secs }
-
--- Handle s en q tijdens het start menu
 input :: Event -> GameState -> IO GameState
 input e gstate = case e of
   EventKey (Char 's') Down _ _ | mode gstate == StartMenu -> return gstate { mode = Playing }
   EventKey (Char 'q') Down _ _ | mode gstate == StartMenu -> exitSuccess
   EventKey (Char 'p') Down _ _ | mode gstate == Playing   -> return gstate { isPaused = not (isPaused gstate) }
   _ | mode gstate == Playing -> return $ (inputKey e . resizeEvent e) gstate
+  _ | mode gstate == Exiting -> return gstate
   _ -> return gstate
 
 resizeEvent :: Event -> GameState -> GameState
